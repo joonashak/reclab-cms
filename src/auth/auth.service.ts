854ef7c0007ec4fad9a52dcba1038fdc26e1ce9a
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-09-03 22:46:22 +0300

    Implement passport-jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+
+    if (user && user.passwordHash === password) {
+      const { passwordHash, ...result } = user;
+      return result;
+    }
+
+    return null;
+  }
+}
