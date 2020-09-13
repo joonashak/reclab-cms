a7a05e43783a9137b8121fc1d9145e8759439e47
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-09-13 20:11:47 +0300

    Throw nicer error

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Page } from 'src/pages/page.entity';
 import { Connection, Repository } from 'typeorm';
@@ -12,9 +12,11 @@ export class SeederService {
   ) {}
 
   async clear(): Promise<void> {
-    if (process.env.NODE_ENV !== 'production') {
-      await this.connection.dropDatabase();
-      await this.connection.synchronize();
+    if (process.env.NODE_ENV === 'production') {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN)
     }
+
+    await this.connection.dropDatabase();
+    await this.connection.synchronize();
   }
 }
