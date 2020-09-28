ae46f963d94aa0f7798d2eaf012684575dfeaa48
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-09-28 21:59:44 +0300

    Add login test

---
+++ test/authentication.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { expect } from 'chai';
+import initializeApp from './utils/initializeApp';
+
+describe('/auth/login', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    app = await initializeApp();
+  });
+
+  it('Can login with valid credentials.', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ username: 'admin', password: '1234' })
+      .expect(201);
+
+    expect(res.body).to.have.property('accessToken');
+  });
+});
