07792b720635ee63008af4324e578105b3f6ce51
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-09-28 16:27:59 +0300

    Refactor tests

--- test/page.e2e-spec.ts
+++ test/page.e2e-spec.ts
@@ -1,36 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
-import pagesSeed from '../src/seeder/data/pages.seed';
-
-// Remove fields that are not returned by the API.
-const pages = pagesSeed.map(page => {
-  const { author, ...rest } = page;
-  const { id, username } = author;
-  return { ...rest, author: { id, username } };
-});
+import initializeApp from './utils/initializeApp';
+import { apiPages } from './utils/apiData';
 
 describe('/page', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-
-    return request(app.getHttpServer())
-      .get('/seeder/reset')
-      .expect(200);
+    app = await initializeApp();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/page')
       .expect(200)
-      .expect(pages);
+      .expect(apiPages);
   });
 });
