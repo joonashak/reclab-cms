0d1b4c6053775b584f13a4c915dac9b4f6d986d4
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-09-28 12:35:08 +0300

    Hash passwords

--- src/seeder/data/users.seed.ts
+++ src/seeder/data/users.seed.ts
@@ -2,6 +2,6 @@ export default [
   {
     id: 'adc86188-4c93-4993-864e-91a6c129ff8a',
     username: 'admin',
-    passwordHash: '1234',
+    passwordHash: '$2b$12$dJZcs736xzUZpGRonHjcs.jjuc4qW6FCbwlLfeaMRWyKteOr/B8vq',
   },
 ];
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
 export class CreateUserDto {
   username: string;
-  passwordHash: string;
+  password: string;
 }
